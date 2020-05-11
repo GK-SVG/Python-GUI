23d59b1a4d851ff69eb7627e811519bcdd0bcce9
gk-svg <gk32239@gmail.com>
2020-05-11 10:03:23 +0530

    python gui using tkinter

--- tkBasicLabels.py
+++ tkBasicLabels.py
@@ -2,7 +2,7 @@
 from tkinter import *
 root = Tk()
 root.geometry("744x133")
-root.title("My GUI With Harry")
+root.title("My GUI")
 
 # Important Label Options
 # text - adds the text
@@ -24,7 +24,7 @@ including two National Film Awards as a film \nproducer, and two Filmfare Awards
 He has a significant following in Asia and the Indian \ndiaspora worldwide, and is cited in the media as one of the most ,
 commercially successful actors of Indian \ncinema. According to the Forbes 2018 list of Top-Paid 100 Celebrity Entertainers in world,
  Khan was \nthe highest ranked Indian with 82nd rank with earnings of $37.7 million.''',
-  bg ="red", fg="white", padx=13, pady=94, font="comicsansms 9 bold", borderwidth=3, relief=SUNKEN)
+  bg ="black", fg="white", padx=13, pady=94, font="comicsansms 9 bold", borderwidth=3, relief=SUNKEN)
 
 # Important Pack options
 # anchor = nw
@@ -34,7 +34,7 @@ commercially successful actors of Indian \ncinema. According to the Forbes 2018 
 # pady
 
 # title_label.pack(side=BOTTOM, anchor ="sw", fill=X)
-title_label.pack(side=LEFT, fill=Y, padx=34, pady=34)
+title_label.pack(side=BOTTOM, fill=X, padx=34, pady=34)
 
 
 root.mainloop()
